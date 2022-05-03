41501086e7caa9f7b355c7ef50826dca7c051f0b
lucianMD <lucian4forever@gmail.com>
2022-05-03 16:38:27 +0200

    JS move to solve

--- jeu.js
+++ jeu.js
@@ -11,6 +11,115 @@ function alea(min, max){ // [0;15[
   return Math.floor((Math.random()*(max-min))+min)
 }
 
+
+function estDedans(coord){
+  if (coord[0]>=0 && coord[0]<=3 && coord[1]>=0 && coord[1] <=3)
+    return true;
+  else 
+    return false;
+}
+
+function voisinsInfini(coordTmp){
+  var voisin1 = [coordTmp[0], coordTmp[1]+1];
+  var voisin2 = [coordTmp[0], coordTmp[1]-1];
+  var voisin3 = [coordTmp[0]+1, coordTmp[1]];
+  var voisin4 = [coordTmp[0]-1, coordTmp[1]];
+return([voisin1, voisin2, voisin3, voisin4])
+}
+
+function voisinsValides(coordTmp){
+  var checkVoisins = voisinsInfini(coordTmp);
+  var bonVoisins = [];
+  for (i = 0; i<4; i++){
+    if (estDedans(checkVoisins[i])) {
+      bonVoisins.push(checkVoisins[i]);
+  }
+}
+  return bonVoisins;
+}
+
+function equalCoord(coord1, coord2){
+  if (coord1[0] == coord2[0] && coord1[1] == coord2[1])
+    return true;
+  else
+    return false;
+}
+
+function checkIfVideInVoisins(lstVoisins, cellule) {
+  for (var i = 0; i<lstVoisins.length; i++){
+      let coord1V = lstVoisins[i][0]; let coord2V = lstVoisins[i][1]; console.log("("+coord1V+","+coord2V+")");
+      let coord1C = cellule[0][0]; let coord2C = cellule[0][1]; console.log("("+coord1C+","+coord2C+")");
+      if (coord1V == coord1C && coord2V == coord2C) return true;
+  }
+  return false;
+}
+
+jQuery.fn.outerHTML = function() {
+  return jQuery('<div />').append(this.eq(0).clone()).html();
+};
+
+function check_and_swap(tuile,styles) {
+  var array = $(".tuile");
+  var array2D = [];
+  var arrayCoord = [];
+
+  for (var i = 0; i<4; i++){
+    for (var j = 0; j<4; j++){
+      arrayCoord.push([i,j]);
+    }
+  }
+  console.table(array);
+  var j = 0;
+  for (let i = 0; i<array.length; i++){
+    array2D.push([arrayCoord[i], array[i].id]);
+  }
+
+  var tVide = 0; // pour trouver la tuile vide 
+  for (tVide = 0; tVide<array2D.length; tVide++){
+    if (array2D[tVide][1] == "15") break;
+    }
+
+  var tmp = 0; // pour trouver l'indice de la tuile cliquee
+  for (tmp = 0; tmp<array2D.length; tmp++){
+    if (array2D[tmp][1] == tuile) break;
+    }
+    console.log("tVide = "+tVide);
+    console.log("tmp = " +tmp);
+    var voisinDeTmp = voisinsValides(array2D[tmp][0]); // on cherche les voisins valides
+    console.table(voisinDeTmp);
+
+  var tuileVide;
+  var tuileEchange;
+  if (checkIfVideInVoisins(voisinDeTmp, array2D[tVide])){
+      saveTuileID = array[tmp].id;
+      saveTuileStyle = styles;
+      saveTuileinnerHTML = array[tmp].innerHTML;
+
+      array[tmp].id = array[tVide].id;
+      array[tmp].style = array[tVide].style;
+      array[tmp].innerHTML = array[tVide].innerHTML;
+      tuileEchange = array[tmp].outerHTML;
+
+      array[tVide].id = saveTuileID;
+      array[tVide].style = saveTuileStyle;
+      array[tVide].innerHTML = saveTuileinnerHTML;
+      tuileVide = array[tVide].outerHTML;
+  }
+  else {
+      tuileVide = array[tVide].outerHTML;
+      tuileEchange = array[tmp].outerHTML;
+  }
+  newArray = [];
+  for (var i = 0; i<array.length; i++){
+      if (i == tVide) { newArray.push(tuileVide) } else
+      if (i == tmp) { newArray.push(tuileEchange) } else
+      newArray.push(array[i].outerHTML);
+  }
+  console.table(newArray);
+  $(".tuile").remove();   
+  $("#puzzlearea").append(newArray);
+}
+
 $(document).ready(function(){
 
   function shuffle () {
@@ -47,16 +156,11 @@ $(document).ready(function(){
     shuffle();
   })
 
-  function check_and_swap() {
-    var array = $(".tuile");
-    var i = 0;
-    for (; i<array.length() && array[i] != this; i++){}
-    
-  }
-
-
-  $("#puzzlearea").on("click", ".tuille", function(){
-    check_and_swap();
+  $("#puzzlearea").on("click", ".tuile", function(){
+    var tuile = $(this).attr('id');
+    var style = $(this).attr('style');
+    check_and_swap(tuile, style);
   })
+
 
 });
